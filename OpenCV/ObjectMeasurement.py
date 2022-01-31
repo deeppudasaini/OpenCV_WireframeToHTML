5a2339c5762211d703a70ce122149904f1a0950c
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2022-01-31 21:50:39 +0545

    Object Size Determined

--- OpenCV/ObjectMeasurement.py
+++ OpenCV/ObjectMeasurement.py
@@ -1,10 +1,11 @@
 import cv2
-import utlis
+import Utils
 
 ###################################
-webcam = True
-path = '1.jpg'
-cap = cv2.VideoCapture(0)
+webcam = False
+
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(10, 160)
 cap.set(3, 1920)
 cap.set(4, 1080)
@@ -13,28 +14,30 @@ wP = 210 * scale
 hP = 297 * scale
 ###################################
 
+
 while True:
     if webcam:
         success, img = cap.read()
     else:
-        img = cv2.imread(path)
+        img = cv2.imread('OpenCV/5.png')
 
-    imgContours, conts = utlis.getContours(img, minArea=50000, filter=4)
+    imgContours, conts = Utils.getContours(img, minArea=50000, filter=4)
     if len(conts) != 0:
         biggest = conts[0][2]
-        # print(biggest)
-        imgWarp = utlis.warpImg(img, biggest, wP, hP)
-        imgContours2, conts2 = utlis.getContours(imgWarp,
+
+        imgWarp = Utils.warpImg(img, biggest, wP, hP)
+        imgContours2, conts2 = Utils.getContours(imgWarp,
                                                  minArea=2000, filter=4,
                                                  cThr=[50, 50], draw=False)
         if len(conts) != 0:
+
             for obj in conts2:
                 cv2.polylines(imgContours2, [obj[2]], True, (0, 255, 0), 2)
-                nPoints = utlis.reorder(obj[2])
+                nPoints = Utils.reorder(obj[2])
                 nW = round(
-                    (utlis.findDis(nPoints[0][0]//scale, nPoints[1][0]//scale)/10), 1)
+                    (Utils.findDis(nPoints[0][0]//scale, nPoints[1][0]//scale)/10), 1)
                 nH = round(
-                    (utlis.findDis(nPoints[0][0]//scale, nPoints[2][0]//scale)/10), 1)
+                    (Utils.findDis(nPoints[0][0]//scale, nPoints[2][0]//scale)/10), 1)
                 cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
                                 (255, 0, 255), 3, 8, 0, 0.05)
                 cv2.arrowedLine(imgContours2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
@@ -44,6 +47,12 @@ while True:
                             (255, 0, 255), 2)
                 cv2.putText(imgContours2, '{}cm'.format(nH), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5,
                             (255, 0, 255), 2)
+                f = open("final.txt", "w+")
+                f.write(
+                    "<!DOCTYPE html><html><head><title>Test</title></head><body>")
+
+                f.write("</body></html>")
+                f.close()
         cv2.imshow('A4', imgContours2)
 
     img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
